24c348c0e6d4f774dc5f89edb71f36032be1a088
xuehrs <736363980@qq.com>
2018-07-03 23:58:13 +0800

    upadte readme.md+6

--- pandoc-test/mmake.sh
+++ pandoc-test/mmake.sh
@@ -8,11 +8,7 @@
 
 #https://www.jianshu.com/p/4721ddd27027
 #将Markdown文件生成目录，生成完毕后要自行校验！默认生成三级目录。
-#--toc-depth=4：表示将文档生成四级目录
+#--toc-depth=4：表示将文档生成四级目录,如果加大这个数字，可以解决标题空格转换失败问题。
 echo 正在为Markdown文件生成目录，生成完毕后请自行校验！
-cd ..
-#该命令会覆盖pandoc-test/README-max.md文件
-cp README-max.md pandoc-test/
-cd pandoc-test
-pandoc -s --toc --toc-depth=4 README-max.md -o README-max-$(date +%Y年%m月%d日%H时%I分%S秒).md
+pandoc -s --toc --toc-depth=6 README-max.md -o README-max-$(date +%Y年%m月%d日%H时%I分%S秒).md
 
